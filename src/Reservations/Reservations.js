38ba014f612053f789aee2c74a91f4fa2f89c547
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-12 11:25:35 -0700

    Change references and add key

--- src/Reservations/Reservations.js
+++ src/Reservations/Reservations.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import './Reservations.scss';
 
 export const Reservations = (props) => {
-  return props.reservationData.map( ({ id, name, date, time, number }) => {
+  return props.reservations.map( ({ id, name, date, time, number }) => {
     return (
-      <div className='reservation'>
+      <div className='reservation' key={ id }>
         <h3 className='reservation__name'>{ name }</h3>
         <p className='reservation__date'>{ date }</p>
         <p className='reservation__time'>{ time } PM</p>
