6868a1621dd04f05e174c89a4a544fb5cb9b831b
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-12 11:25:07 -0700

    Add logic for adding reservation to state

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './App.scss';
 import { Reservations } from '../Reservations/Reservations.js';
-import { fetchReservations } from '../apiCalls/apiCalls.js';
+import { fetchReservations, postReservation } from '../apiCalls/apiCalls.js';
+import Form from '../Form/Form.js';
 
 class App extends Component {
   constructor() {
@@ -18,6 +19,13 @@ class App extends Component {
   getReservations() {
     fetchReservations()
       .then(data => this.setState({reservations: data}))
+      .catch(error => console.log(error))
+  }
+
+  addReservation = (newReservation) => {
+    postReservation(newReservation)
+      .then(data => this.setState({reservations: [...this.state.reservations, data]}))
+      .catch(error => console.log(error))
   }
 
   componentDidMount() {
@@ -29,10 +37,10 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form addReservation={this.addReservation}/>
         </div>
         <div className='resy-container'>
-          <Reservations reservationData={this.state.reservations} />
+          <Reservations reservations={this.state.reservations} />
         </div>
       </div>
     )
