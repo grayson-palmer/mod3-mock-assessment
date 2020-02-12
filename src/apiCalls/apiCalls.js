e3c1931c05d9fa46f39732a6f4ebda5fcba54cb5
Grayson Palmer <gpalmer1385@gmail.com>
2020-02-12 11:26:11 -0700

    Add api logic to post reservation

--- src/apiCalls/apiCalls.js
+++ src/apiCalls/apiCalls.js
@@ -7,3 +7,20 @@ export const fetchReservations = () => {
       return response.json()
     })
 }
+
+export const postReservation = (newReservation) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(newReservation),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return fetch('http://localhost:3001/api/v1/reservations', options)
+    .then(response => {
+      if(!response.ok) {
+        throw Error('Problem while posting reservation')
+      }
+      return response.json()
+    })
+}
